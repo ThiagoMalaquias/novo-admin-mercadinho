a070f0b23ab28a04eacd84e937331b45ace947f7
Thiago Malaquias <tammalaquias@gmail.com>
2026-01-23 12:19:14 -0300

    aplicando busca por grupo

--- app/controllers/api/v1/produtos_controller.rb
+++ app/controllers/api/v1/produtos_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::ProdutosController < Api::V1::ApplicationController
   def index
-    # Subquery para produtos com estoque disponível > 0
     produtos_com_estoque = Estoque.where(filial_id: @filial.id)
                                    .group(:produto_id)
                                    .having("COALESCE(SUM(CASE WHEN acao = 'entrada' THEN quantidade ELSE -quantidade END), 0) > 0")
                                    .select(:produto_id)
-    
+
     @produtos = Produto.order(descricao_cupom: :asc)
                        .joins(:filial_produtos)
                        .where(filial_produtos: { status: "ATIVO", filial_id: @filial.id })
@@ -13,5 +12,9 @@ class Api::V1::ProdutosController < Api::V1::ApplicationController
                        .includes(:filial_produtos)
 
     @produtos = @produtos.where(codigo_venda: params[:codigo_barras]) if params[:codigo_barras].present?
+    @produtos = @produtos.where(grupo_produto_id: params[:grupo_produto_id]) if params[:grupo_produto_id].present?
+
+    options = { page: params[:page], per_page: params[:per_page] }
+    @produtos = @produtos.paginate(options)
   end
 end
